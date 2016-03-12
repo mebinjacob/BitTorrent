05e283083c3e440aead566f222c37da14c3163cd
Mebin Jacob <mebinjacob@gmail.com>
2016-03-12 18:35:32 -0500

    log formatting fixed

--- src/main/java/LogFormatter.java
+++ src/main/java/LogFormatter.java
@@ -7,13 +7,13 @@ public class LogFormatter extends Formatter {
 	public String format(LogRecord rc) {
 		StringBuffer buf = new StringBuffer(1000);
 		buf.append(calcDate(rc.getMillis()));
-		buf.append(": ");
+		buf.append(" ");
 		buf.append(rc.getMessage());
 		return buf.toString();
 	}
 
 	private String calcDate(long millisecs) {
-		SimpleDateFormat date_format = new SimpleDateFormat("MMM dd,yyyy HH:mm");
+		SimpleDateFormat date_format = new SimpleDateFormat("MMM dd,yyyy HH:mm:ss");
 		Date resultdate = new Date(millisecs);
 		return date_format.format(resultdate);
 	}
